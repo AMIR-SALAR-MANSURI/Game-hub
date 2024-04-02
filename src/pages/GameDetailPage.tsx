05e20ccda1fc2c1a4753da47c4ecf890f730a908
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-04-02 14:16:08 +0330

    fetch the game detail

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
+
 import { Heading, Spinner, Text } from "@chakra-ui/react";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
+
   const { data: game, isLoading, error } = useGame(slug!);
+  console.log(game);
   if (isLoading) return <Spinner />;
-  if (error || !game) throw error;
+  // if (error || !game) throw error;
   return (
     <>
-      <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <Heading>{game?.name}</Heading>
+      <Text>{game?.description_raw}</Text>
     </>
   );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       {
-        path: "games/:slug ",
+        path: "games/:slug",
         element: <GameDetailPage />,
       },
     ],
