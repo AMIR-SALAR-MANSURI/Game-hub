f036b539c6da60ba82df0f2ffbe14d5976875804
AMIR-SALAR-MANSURI <133604758+AMIR-SALAR-MANSURI@users.noreply.github.com>
2024-04-03 11:44:57 +0330

    build definition item

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publishers } from "./Publishers";
 
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  genres: Genre[];
+  publishers: Publishers[];
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
